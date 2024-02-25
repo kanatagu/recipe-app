97e00d9f69fc136616e7657a3e4fcd208553c266
Kana Taguchi <mukrabwy103@gmail.com>
2024-02-24 16:55:51 -0800

    Change upload function

--- lib/actions/upload-image.ts
+++ lib/actions/upload-image.ts
@@ -6,27 +6,38 @@ import { UploadApiResponse } from 'cloudinary';
 export const uploadImage: (
   formData: FormData
 ) => Promise<UploadApiResponse | undefined> = async (formData) => {
-  const image = formData.get('image') as File;
-  const arrayBuffer = await image.arrayBuffer();
-  const buffer = new Uint8Array(arrayBuffer);
+  const image = (await formData.get('image')) as File;
+  const fileBuffer = await image.arrayBuffer();
 
-  const res = await new Promise(
-    (resolve: (value: UploadApiResponse | undefined) => void, reject) => {
-      cloudinary.uploader
-        .upload_stream(
-          {
+  const mime = image.type;
+  const encoding = 'base64';
+  const base64Data = Buffer.from(fileBuffer).toString('base64');
+  const fileUri = 'data:' + mime + ';' + encoding + ',' + base64Data;
+
+  try {
+    const uploadToCloudinary: () => Promise<UploadApiResponse> = () => {
+      return new Promise((resolve, reject) => {
+        cloudinary.uploader
+          .upload(fileUri, {
             folder: 'recipe-app',
-          },
-          function (error, result) {
-            if (error) {
-              reject(error);
-              return;
-            }
+            invalidate: true,
+          })
+          .then((result) => {
+            console.log(result);
             resolve(result);
-          }
-        )
-        .end(buffer);
-    }
-  );
-  return res;
+          })
+          .catch((error) => {
+            console.log(error);
+            reject(error);
+          });
+      });
+    };
+
+    const result = await uploadToCloudinary();
+
+    return result;
+  } catch (error) {
+    console.error('Error uploading image', error);
+    return;
+  }
 };
